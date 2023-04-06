171c07975678985e5a5795b8684251e320698515
Igor <eng.igorwatanabe@gmail.com>
2023-04-06 16:00:28 -0300

    item 3 finalizados lint

--- src/App.js
+++ src/App.js
@@ -5,30 +5,11 @@ import Card from './components/Card';
 
 class App extends React.Component {
   render() {
-    // const {
-    //   cardName,
-    //   cardDescription,
-    //   cardAttr1,
-    //   cardAttr2,
-    //   cardAttr3,
-    //   cardImage,
-    //   cardRare,
-    //   cardTrunfo,
-    // } = this.props;
     return (
       <div>
         <h1>Tryunfo</h1>
         <Form />
         <Card />
-          // cardName={ cardName }
-          // cardDescription={ cardDescription }
-          // cardAttr1={ cardAttr1 }
-          // cardAttr2={ cardAttr2 }
-          // cardAttr3={ cardAttr3 }
-          // cardImage={ cardImage }
-          // cardRare={ cardRare }
-          // cardTrunfo={ cardTrunfo }
-        
       </div>
     );
   }
